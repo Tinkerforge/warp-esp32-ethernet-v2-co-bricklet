322482487bb154cbc6fcd6d6919fe1c01290df55
Olaf Lüke <olaf@tinkerforge.com>
2025-07-09 14:35:59 +0200

    Add watchdog stub

---
+++ software/src/watchdog.c
@@ -0,0 +1,42 @@
+/* warp-esp32-ethernet-v2-co-bricklet
+ * Copyright (C) 2025 Olaf Lüke <olaf@tinkerforge.com>
+ *
+ * watchdog.c: Driver for Watchdog
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include "watchdog.h"
+
+#include "configs/config_watchdog.h"
+#include "bricklib2/hal/system_timer/system_timer.h"
+#include "bricklib2/hal/i2c_fifo/i2c_fifo.h"
+#include "bricklib2/logging/logging.h"
+
+Watchdog watchdog;
+
+void watchdog_init(void) {
+	memset(&watchdog, 0, sizeof(Watchdog));
+		const XMC_GPIO_CONFIG_t config_high = {
+		.mode             = XMC_GPIO_MODE_OUTPUT_PUSH_PULL,
+		.output_level     = XMC_GPIO_OUTPUT_LEVEL_LOW,
+	};
+	XMC_GPIO_Init(ESP_EN_PIN, &config_high);
+}
+
+void watchdog_tick(void) {
+
+}
